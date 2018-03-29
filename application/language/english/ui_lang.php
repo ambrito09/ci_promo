fe159bd5bf07ceb107090b0f0ac406b43f876f4c
Brito <ambrito@zoho.com>
2018-03-28 21:28:10 -0400

    Adding translation files.

---
+++ application/language/english/ui_lang.php
@@ -0,0 +1,6 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['key'] = "Text to show";
+
+/* End of file messages.php */
+/* Location: ./system/application/language/english/messages.php */
