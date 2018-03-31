258ae432842c06bd0c6dcd94aa010a8c32466755
Brito <ambrito@zoho.com>
2018-03-31 19:18:00 -0400

    Added Profile View

---
+++ application/views/home/profile.php
@@ -0,0 +1,63 @@
+<link href="<?=base_url()?>assets/css/style.default.css" rel="stylesheet">
+
+<div class="container margin-top-20">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row margin-bottom-10" id="productMain">
+                <div class="col-sm-6">
+                    <div id="mainImage">
+                        <img src="<?=base_url()?>assets/img/detailbig1.jpg" alt="" class="img-fluid">
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="box">
+                        <h1 class="text-center">White Blouse Armani</h1>
+
+                    </div>
+                    <div class="row" id="thumbs">
+                        <div class="col-4">
+                            <a href="<?=base_url()?>assets/img/detailsquare.jpg" class="thumb">
+                                <img src="<?=base_url()?>assets/img/detailsquare.jpg" alt="" class="img-thumbnail">
+                            </a>
+                        </div>
+                        <div class="col-4">
+                            <a href="<?=base_url()?>assets/img/detailsquare2.jpg" class="thumb">
+                                <img src="<?=base_url()?>assets/img/detailsquare2.jpg" alt="" class="img-thumbnail">
+                            </a>
+                        </div>
+                        <div class="col-4">
+                            <a href="<?=base_url()?>assets/img/detailsquare3.jpg" class="thumb">
+                                <img src="<?=base_url()?>assets/img/detailsquare3.jpg" alt="" class="img-thumbnail">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="box" id="details">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+
+                <hr>
+                <!--div class="social">
+                    <h4>Show it to your friends</h4>
+                    <p>
+                        <a href="#" class="external facebook" data-animate-hover="pulse"><i class="fa fa-facebook"></i></a>
+                        <a href="#" class="external gplus" data-animate-hover="pulse"><i class="fa fa-google-plus"></i></a>
+                        <a href="#" class="external twitter" data-animate-hover="pulse"><i class="fa fa-twitter"></i></a>
+                        <a href="#" class="email" data-animate-hover="pulse"><i class="fa fa-envelope"></i></a>
+                    </p>
+                </div-->
+            </div>
+        </div>
+    </div>
+</div>
+
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -24,10 +24,56 @@
 
     $('.right').click(function() {
         owl.trigger('next.owl.carousel');
-    })
+    });
 // Go to the previous item
     $('.left').click(function() {
         owl.trigger('prev.owl.carousel');
-    })
+    });
 
-})(jQuery)
+    productDetailGallery(4000);
+
+    $('.product-slider').owlCarousel({
+        navigation: true, // Show next and prev buttons
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        afterInit: function() {
+            $('.product-slider .item').css('visibility', 'visible');
+        }
+    });
+
+})(jQuery);
+
+/* product detail gallery */
+function productDetailGallery(confDetailSwitch) {
+    $('.thumb:first').addClass('active');
+    timer = setInterval(autoSwitch, confDetailSwitch);
+    $(".thumb").click(function(e) {
+
+            switchImage($(this));
+            clearInterval(timer);
+            timer = setInterval(autoSwitch, confDetailSwitch);
+            e.preventDefault();
+        }
+    );
+    $('#mainImage').hover(function() {
+        clearInterval(timer);
+    }, function() {
+        timer = setInterval(autoSwitch, confDetailSwitch);
+    });
+
+    function autoSwitch() {
+        var nextThumb = $('.thumb.active').closest('div').next('div').find('.thumb');
+        if (nextThumb.length == 0) {
+            nextThumb = $('.thumb:first');
+        }
+        switchImage(nextThumb);
+    }
+
+    function switchImage(thumb) {
+
+        $('.thumb').removeClass('active');
+        var bigUrl = thumb.attr('href');
+        thumb.addClass('active');
+        $('#mainImage img').attr('src', bigUrl);
+    }
+}
