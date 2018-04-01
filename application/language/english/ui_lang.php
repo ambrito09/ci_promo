7e46503665c367fc45479c4cac33371c2e2814d8
Brito <ambrito@zoho.com>
2018-03-31 21:22:58 -0400

    Internatiolization keys in language files.

--- application/language/english/ui_lang.php
+++ application/language/english/ui_lang.php
@@ -1,6 +1,28 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$lang['key'] = "Text to show";
+/* MENU LINKS */
+$lang['site_name'] = "Promotion";
+$lang['link_login'] = "Login";
+$lang['link_myprofile'] = "My User";
+/* HOME PAGE LINKS*/
+$lang['vip_title'] = "VIP";
+$lang['announces_title'] = "Announces";
+$lang['details_title'] = "Details";
+/* LOGIN PAGE */
+$lang['login_page_title'] = "Signin";
+$lang['username_placeholder'] = "Username";
+$lang['password_placeholder'] = "Password";
+$lang['login_button'] = "Sign In";
+$lang['lostpassword_link'] = "Lost your password?";
+$lang['newtosite_link'] = "New to site?";
+$lang['createaccount_link'] = "Create Account";
+/* REGISTRATION PAGE */
+$lang['registrationpage_title'] = "User Registration";
+$lang['fullname_placeholder'] = "Full name";
+$lang['user_placeholder'] = "User";
+$lang['email_placeholder'] = "Email";
+$lang['retype_placeholder'] = "Retype password";
+$lang['register_button'] = "Register";
 
 /* End of file messages.php */
 /* Location: ./system/application/language/english/messages.php */
--- application/language/italian/ui_lang.php
+++ application/language/italian/ui_lang.php
@@ -1,6 +1,28 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$lang['key'] = "Text to show";
+/* MENU LINKS */
+$lang['site_name'] = "iPromotion";
+$lang['link_login'] = "iLogin";
+$lang['link_myprofile'] = "iMy User";
+/* HOME PAGE LINKS*/
+$lang['vip_title'] = "iVIP";
+$lang['announces_title'] = "iAnnounces";
+$lang['details_title'] = "iDetails";
+/* LOGIN PAGE */
+$lang['login_page_title'] = "iSignin";
+$lang['username_placeholder'] = "iUsername";
+$lang['password_placeholder'] = "iPassword";
+$lang['login_button'] = "iSign In";
+$lang['lostpassword_link'] = "iLost your password?";
+$lang['newtosite_link'] = "iNew to site?";
+$lang['createaccount_link'] = "iCreate Account";
+/* REGISTRATION PAGE */
+$lang['registrationpage_title'] = "iUser Registration";
+$lang['fullname_placeholder'] = "iFull name";
+$lang['user_placeholder'] = "iUser";
+$lang['email_placeholder'] = "iEmail";
+$lang['retype_placeholder'] = "iRetype password";
+$lang['register_button'] = "iRegister";
 
 /* End of file messages.php */
-/* Location: ./system/application/language/english/messages.php */
+/* Location: ./system/application/language/italian/messages.php */
