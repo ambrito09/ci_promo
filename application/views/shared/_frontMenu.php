b108665c7956945b4310a2414cfbca4ecd8f365f
Brito <ambrito@zoho.com>
2018-04-12 21:48:21 -0400

    Update link reference.

--- application/views/shared/_frontMenu.php
+++ application/views/shared/_frontMenu.php
@@ -1,7 +1,7 @@
 <h1 class="logo"><a href="<?=site_url()?>"><?=$this->lang->line('site_name')?></a></h1>
 <i class="fa fa-arrow-right menu-close"></i>
 <?php if ($this->session->userdata("loggedIn") === TRUE){?>
-    <a href="<?=site_url("home/myprofile")?>"><?=$this->lang->line('link_myprofile')?></a>
+    <a href="<?=site_url("profile/myprofile")?>"><?=$this->lang->line('link_myprofile')?></a>
     <a href="<?=site_url("home/logout")?>"><?=$this->lang->line('link_logout')?></a>
 <?php } else {?>
     <a href="<?=site_url("home/login")?>"><?=$this->lang->line('link_login')?></a>
